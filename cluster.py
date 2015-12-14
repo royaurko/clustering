72371727792649f67e0273b035096119544fa512
Aurko Roy <roy.aurko@gmail.com>
2015-12-13 20:32:09 -0500

    clean up some bugs

--- cluster.py
+++ cluster.py
@@ -11,8 +11,6 @@ import os
 import argparse
 import csv
 import pickle
-import random
-num_cpu = multiprocessing.cpu_count()
 
 
 class NoDaemonProcess(multiprocessing.Process):
@@ -87,23 +85,22 @@ def get_distance(X, S, i):
     """
     if i in S:
         return None
-    S = list(S)
-    with Pool(processes=num_cpu) as pool:
-        func = partial(compute_norm, X, i)
-        norms = pool.map(func, S)
-    d = sum(norms)
+    d = 0
+    for j in S:
+        d += norm(X[i] - X[j])
     d /= len(S)
     return i, d
 
 
-def set_distances(X, S):
+def set_distances(X, S, num_workers):
     """ Return the elements outside S sorted by distance to S
     :param X: Data matrix
     :param S: set of points
+    :param num_workers: Number of workers
     :return: Elements outside S sorted by distance to S
     """
     n = len(X)
-    with Pool(processes=num_cpu) as pool:
+    with Pool(processes=num_workers) as pool:
         func = partial(get_distance, X, S)
         dist = pool.map(func, range(n))
         pool.close()
@@ -114,15 +111,16 @@ def set_distances(X, S):
     return elem
 
 
-def get_thresholds(X, minsize, i):
+def get_thresholds(X, minsize, num_workers, i):
     """ Get the threshold cluster for the i^{th} element of X
     :param X: Data set
     :param D: Dictionary
     :param minsize: Minimum size of a cluster
+    :param num_workers: Number of workers
     :param i: element is X[i]
     :return:
     """
-    elem = set_distances(X, {i})
+    elem = set_distances(X, {i}, num_workers)
     thresholds = []
     for j in range(minsize - 1, len(elem)):
         cluster = set(elem[:j])
@@ -131,21 +129,22 @@ def get_thresholds(X, minsize, i):
     return thresholds, i, elem
 
 
-def threshold(X, e, g, s, k):
+def threshold(X, e, g, s, k, num_workers):
     """ Get all threshold clusters (algorithm 7, lines 1-6)
     :param X: Data matrix
     :param e: lower bound on fractional size of each cluster
     :param g: lower bound on fractional size of a set inside own cluster for which stability holds
     :param s: lower bound on fractional size of a set outside own cluster for which stability holds
     :param k: Number of clusters
+    :param num_workers: Number of workers
     :return: Threshold clusters
     """
     print('Populating list with all threshold clusters')
     start = time.clock()
     n = len(X)
     minsize = int(e * n)
-    with Pool(processes=num_cpu) as pool:
-        func = partial(get_thresholds, X, minsize)
+    with Pool(processes=num_workers) as pool:
+        func = partial(get_thresholds, X, minsize, num_workers)
         items = pool.map(func, range(n))
         pool.close()
         pool.join()
@@ -155,7 +154,7 @@ def threshold(X, e, g, s, k):
     end = time.clock()
     print('Length of L = ', len(L))
     print('time = ', (end - start))
-    return refine(L, X, D, e, g, s, k)
+    return refine(L, X, D, e, g, s, k, num_workers)
 
 
 def refine_individual(D, T, t, S):
@@ -178,7 +177,7 @@ def refine_individual(D, T, t, S):
     return B
 
 
-def refine(L, X, D, e, g, s, k):
+def refine(L, X, D, e, g, s, k, num_workers):
     """ Throw out bad points (algorithm 7, lines 7-17)
     :param L: List of subsets
     :param X: Data matrix
@@ -187,6 +186,7 @@ def refine(L, X, D, e, g, s, k):
     :param g: lower bound on fractional size of a set inside own cluster for which stability holds
     :param s: lower bound on fractional size of a set outside own cluster for which stability holds
     :param k: Number of clusters
+    :param num_workers: Number of workers
     :return: Refined clusters
     """
     print('Getting rid of bad points')
@@ -195,7 +195,7 @@ def refine(L, X, D, e, g, s, k):
     n = len(X)
     T = int((e - 2*g - s*k) * n)
     t = int((e - g) * n)
-    with Pool(processes=num_cpu) as pool:
+    with Pool() as pool:
         func = partial(refine_individual, D, T, t)
         L = pool.map(func, L)
         pool.close()
@@ -203,37 +203,40 @@ def refine(L, X, D, e, g, s, k):
     end = time.clock()
     print('Length of L on end = ', len(L))
     print('time = ', (end - start))
-    return grow(L, X, g)
+    return grow(L, X, g, num_workers)
 
 
-def grow_individual(X, t, A):
+def grow_individual(X, t, num_workers, A):
     """ Grow an individual candidate cluster
     :param X: Data set
     :param t: Threshold
     :param A: Candidate set
+    :param num_workers: Number of workers
     :return:
     """
-    elem = set_distances(X, A)
+    elem = set_distances(X, A, num_workers)
     tmp = set(elem[:t])
     A = A.union(tmp)
     return A
 
 
-def grow(L, X, g):
+def grow(L, X, g, num_workers):
     """ Get back good points (algorithm 7, lines 18-21)
-    :param L:
-    :param X:
-    :param g:
-    :return:
+    :param L: The list of candidate clusters
+    :param X: Data set
+    :param g: Parameter on stability
+    :return: Refined list of candidate clusters
     """
     print('Getting back good points')
     print('Length of L at start = ', len(L))
     start = time.clock()
     n = len(X)
     t = int(g*n)
-    with Pool(processes=num_cpu) as pool:
-        func = partial(grow_individual, X, t)
+    with Pool(processes=num_workers) as pool:
+        func = partial(grow_individual, X, t, num_workers)
         L = pool.map(func, L)
+        pool.close()
+        pool.join()
     end = time.clock()
     print('Length of L = ', len(L))
     print('time = ', (end - start))
@@ -264,16 +267,26 @@ def non_laminar(L, i):
     indices = list()
     for j in range(i + 1, len(L)):
         intersection = L[i].intersection(L[j])
-        if len(intersection) > 0:
+        if intersection:
             if L[i].issubset(L[j]) or L[j].issubset(L[i]):
                 continue
             else:
-                indices.append(j)
-    tuples = [(i, index) for index in indices]
-    return tuples
+                t = (i, j)
+                indices.append(t)
+    return indices
 
 
-def mark_non_laminar(L, X, e, g, s, t):
+def mark_non_laminar(L, X, e, g, s, num_workers, t):
+    """
+    :param L: List of candidate clusters
+    :param X: Data set
+    :param e: parameter on size of clusters
+    :param g: parameter on similarity condition
+    :param s: parameter on similarity condition
+    :param num_workers: Number of workers
+    :param t: Tuple (i, j) where L[i] and L[j] are non-laminar
+    :return: None, mark either L[i] or L[j] None
+    """
     i, j = t[0], t[1]
     if L[i] is None or L[j] is None:
         return
@@ -290,7 +303,7 @@ def mark_non_laminar(L, X, e, g, s, t):
     else:
         # Intersection is small
         v = intersection.pop()
-        elem = set_distances(X, {v})
+        elem = set_distances(X, {v}, num_workers)
         t = int((e - g) * n)
         elem = elem[:t]
         int1 = len(L[i].intersection(elem))
@@ -301,7 +314,7 @@ def mark_non_laminar(L, X, e, g, s, t):
             L[i] = None
 
 
-def laminar(L, X, e, g, s):
+def laminar(L, X, e, g, s, num_workers):
     """ Make family laminar (Algorithm 9)
     :param L: List of subsets
     :param X: The data set
@@ -314,105 +327,45 @@ def laminar(L, X, e, g, s):
     start = time.clock()
     n = len(X)
     print('Computing pairs of non-laminar sets')
-    with Pool(processes=num_cpu) as pool:
+    with Pool(processes=num_workers) as pool:
         func = partial(non_laminar, L)
-        intersections = pool.map(func, range(n-1))
+        intersections = pool.map(func, range(len(L)-1))
+        pool.close()
+        pool.join()
     end = time.clock()
-    intersections = set([item for sublist in intersections for item in sublist])
+    intersections = [item for sub_list in intersections for item in sub_list]
     print('Length of intersections = ', len(intersections))
     print('time = ', end - start)
     print('Removing non-laminar pairs')
     start = time.clock()
-    for t in intersections:
-        mark_non_laminar(L, X, e, g, s, t)
-    indicator = map(lambda x: 1 if x is None else 0, L)
-    count = sum(indicator)
-    print('Count of Nones = ', count)
+    with Pool(processes=num_workers) as pool:
+        func = partial(mark_non_laminar, L, X, e, g, s, num_workers)
+        pool.map(func, intersections)
+        pool.close()
+        pool.join()
     L = [item for item in L if item is not None]
     end = time.clock()
-    print('time = ', (end - start))
     print('Length of list after removing non-laminar pairs = ', len(L))
+    print('time = ', (end - start))
     return L
 
 
-def non_laminar_pairs(L):
-    """ Return two sets C1 and C2 in L that are not laminar, else return None
-    :param L: List of subsets
-    :return: Two set indices
-    """
-    while True:
-        i = random.randint(0, len(L)-1)
-        j = random.randint(i + 1, len(L))
-        intersection = L[i].intersection(L[j])
-        if len(intersection) > 0:
-            if L[i].issubset(L[j]) or L[j].issubset(L[i]):
-                continue
-            else:
-                return i, j
-    return
-
-
-def sequential_laminar(L, X, e, g, s):
-    """ Make family laminar (Algorithm 9)
-    :param L: List of subsets
-    :param X: The data set
-    :param e: lower bound on the fractional size of every cluster
-    :param g: lower bound on the fractional size of every set in own cluster for which stability holds
-    :param s: lower bound on the fractional size of every set in outside cluster for which stability holds
-    :return: Laminar list
-    """
-    print('Making the list laminar (sequential)')
-    print('Length of L at start = ', len(L))
-    start = time.clock()
-    n = len(X)
-    S = non_laminar_pairs(L)
-    while S is not None:
-        i = S[0]
-        j = S[1]
-        intersection = L[i].intersection(L[j])
-        if len(intersection) > int(s * n):
-            A = intersection
-            C1 = L[i].difference(A)
-            C2 = L[j].difference(A)
-            if inverse_similarity(X, A, C1) <= inverse_similarity(X, A, C2):
-                del L[j]
-            else:
-                del L[i]
-        else:
-            # Intersection is small
-            v = intersection.pop()
-            elem = set_distances(X, {v})
-            t = int((e - g) * n)
-            elem = elem[:t]
-            int1 = len(L[i].intersection(elem))
-            int2 = len(L[j].intersection(elem))
-            if int1 >= int2:
-                del L[j]
-            else:
-                del L[i]
-        S = non_laminar_pairs(L)
-    end = time.clock()
-    print('Length of L at end = ', len(L))
-    print('time = ', end - start)
-    return L
-
-
-def prune(L, tcluster, k, label):
+def prune(L, target_cluster, k, label):
     """ Given a laminar list and a target cluster return minimum error
     :param L: Laminar list
-    :param tcluster: target cluster
+    :param target_cluster: target cluster
     :param k: number of clusters
     :param label: label of every element
     :return:
     """
     print('Pruning the tree for the best cluster')
-    if len(L) == 0:
-        ''' Empty list'''
-        return error(label, tcluster), label
+    if not L:
+        #  Empty list
+        return error(label, target_cluster), label
     if len(L) == 1:
         for i in L[0]:
             label[i] = k
-        return error(label, tcluster), label
+        return error(label, target_cluster), label
     if k == 1:
         # Not enough labels
         A = set()
@@ -420,7 +373,7 @@ def prune(L, tcluster, k, label):
             A.union(item)
         for i in A:
             label[i] = k
-        return error(label, tcluster), label
+        return error(label, target_cluster), label
     # compute cost of including L[0] and not including L[0]
     A = L[0]
     new_list = list()
@@ -435,8 +388,8 @@ def prune(L, tcluster, k, label):
             new_list.append(L[i])
     for i in A:
         inclusion_label[i] = k
-    inclusion_error = prune(new_list, tcluster, k-1, inclusion_label)
-    non_inclusion_error = prune(L[1:], tcluster, k, label)
+    inclusion_error = prune(new_list, target_cluster, k-1, inclusion_label)
+    non_inclusion_error = prune(L[1:], target_cluster, k, label)
     if inclusion_error[0] < non_inclusion_error[0]:
         result = inclusion_error[0]
         label = inclusion_label
@@ -445,12 +398,13 @@ def prune(L, tcluster, k, label):
     return result, label
 
 
-def test(X, tcluster, k, e):
+def test(X, target_cluster, k, e, num_workers):
     """ Test error on a data set
     :param X: Data  matrix
-    :param tcluster: Target clusters
+    :param target_cluster: Target clusters
     :param k: Number of target clusters
     :param e: Between (0, 1) represents minimum size of a cluster
+    :param num_workers: Number of workers
     :return: None, print results
     """
     k = int(k)
@@ -462,7 +416,7 @@ def test(X, tcluster, k, e):
     Z.append(hac.linkage(y, method='average'))
     Z.append(hac.linkage(X, method='ward'))
     other_clusters = [hac.fcluster(x, k, 'maxclust') for x in Z]
-    errors = [error(x, tcluster) for x in other_clusters]
+    errors = [error(x, target_cluster) for x in other_clusters]
     error_dict = {'single linkage': errors[0], 'complete linkage': errors[1], 'average linkage': errors[2], 'ward': errors[3]}
     s = (0.8*e)/(2*k + 1)
     g = 0.8*0.2*e
@@ -470,32 +424,20 @@ def test(X, tcluster, k, e):
     print('e = ', e)
     print('g = ', g)
     print('s = ', s)
-    # L = threshold(X, e, g, s, k)
-    f = open('prelaminar.pkl', 'rb')
-    L = pickle.load(f)
-    f.close()
-    # parallel_L = laminar(L, X, e, g, s)
-    #f = open('prelaminar.pkl', 'rb')
-    #L = pickle.load(f)
-    #f.close()
-    seq_L = sequential_laminar(L, X, e, g, s)
-    f = open('laminar.pkl', 'wb')
-    t = {'parallel': [], 'seq': seq_L}
-    pickle.dump(t, f)
-    f.close()
-    return
-    L = laminar(L, X, e, g, s)
+    L = threshold(X, e, g, s, k, num_workers)
+    L = laminar(L, X, e, g, s, num_workers)
     print('Length of L = ', len(L))
     label = [1]*len(X)
-    pruned = prune(L, tcluster, k, label)
+    pruned = prune(L, target_cluster, k, label)
     error_dict['threshold'] = pruned[0]
     return error_dict
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data', help='Data file')
     parser.add_argument('-l', '--label', type=int, default=0, help='Column of labels')
+    parser.add_argument('-n', '--num_workers', type=int, default=1, help='Number of workers')
     args = parser.parse_args()
     file_name = args.data
     result = 'results.pkl'
@@ -511,7 +453,7 @@ if __name__ == '__main__':
     X = np.array(X, dtype=float)
     print('Shape of X = ', X.shape)
     k = len(set(target_cluster))
-    error_dict = test(X, target_cluster, k, 1/(2*k))
+    error_dict = test(X, target_cluster, k, 1/(2*k), args.num_workers)
     error_dict = str(error_dict) + '\n'
     d = dict()
     if os.path.exists(result):
@@ -527,3 +469,7 @@ if __name__ == '__main__':
     else:
         d[args.data] = [error_dict]
     f.close()
+
+
+if __name__ == '__main__':
+    main()
